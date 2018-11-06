1e6d3a3d7fe37beaa12013ef5bdde6e3cc13dc15
misbee <bosseeb03@gmail.com>
2018-11-06 11:48:26 +0700

    [Edit] word

--- src/components/Loginscreen.js
+++ src/components/Loginscreen.js
@@ -8,7 +8,7 @@ class Loginscreen extends React.Component {
          <a-scene>
       <a-sky src="/Final01.jpg" rotation="0 -130 0"></a-sky>
 
-      <a-text font="kelsonsans" value="Puy de Sancy, France" width="6" position="-2.5 0.25 -1.5"
+      <a-text font="kelsonsans" value="Puy" width="6" position="-2.5 0.25 -1.5"
               rotation="0 15 0"></a-text>
     </a-scene>
       </div>
