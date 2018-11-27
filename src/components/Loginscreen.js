1623f3e482c275931c3155d5604350a879c87511
DESKTOP-EI0EKV6\GUYS <worawutgg@outlook.co.th>
2018-11-27 13:45:01 +0700

    test pull

--- src/components/Loginscreen.js
+++ src/components/Loginscreen.js
@@ -6,10 +6,14 @@ class Loginscreen extends React.Component {
     return (
       <div>
          <a-scene>
+         
       <a-sky src="/Final01.jpg" rotation="0 -130 0"></a-sky>
 
       <a-text font="kelsonsans" value="Puy de Sancy, France" width="6" position="-2.5 0.25 -1.5"
               rotation="0 15 0"></a-text>
+               <a-text font="kelsonsans" value="Puy de Sancy, France" width="6" position="-1.5 0.50 -5.5"
+              rotation="0 15 0"></a-text>
+           
     </a-scene>
       </div>
      
