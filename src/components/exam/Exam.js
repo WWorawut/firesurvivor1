bdcd102deb72db7c9f53255fde53ba1077b84a71
Natnicha Cheunprapanusorn <natnicha.c@keenpub.local>
2018-12-14 15:01:54 +0700

    [Edit] exam

--- src/components/exam/Exam.js
+++ src/components/exam/Exam.js
@@ -383,9 +383,6 @@ class Exam extends React.Component {
             <BackTop>
             <div className="ant-back-top-inner">UP</div>
             </BackTop>
-            Scroll down to see the bottom-right
-            <strong style={{ color: '#1088e9' }}> blue </strong>
-            button.
         </div>
       
       </div>
